ca76e388a13939baa86db11035c904d7d4bddd46
Conrado Lev <conradolevanti@gmail.com>
2023-05-26 17:04:50 -0300

    Correccion de responsive y validacion de formulario por JS

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,32 +1,59 @@
 
 const nombre = document.getElementById("name");
+const apellido = document.getElementById("lastname");
 const mail = document.getElementById("email");
-const tel = document.getElementById("tel");
-const hora = document.getElementById("time");
-const dia = document.getElementById("date");
-const form =document.getElementById("form");
+const mensajes = document.getElementById("mensaje");
+const form = document.getElementById("form");
+const error = document.getElementById("form_error")
 
 
-function validarmail(mail){
-    var regexEmail = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
-    var Valido = regexEmail.test(mail);
+ form.addEventListener("submit", e=>{
+    e.preventDefault()
+    let regexEmail = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+    let ok = false
+    let errors = ""
+    form_error.innerHTML= ""
 
-    if(Valido==false){
-            alert("Correo no valido") }
-}
+      if(nombre.value.length <4){
+        errors += 'INGRESAR NOMBRE COMPLETO<br>';
+        ok = true
+      }
 
+      if(apellido.value.length <4){
+        errors += 'INGRESAR APELLIDO COMPLETO<br>';
+        ok = true
+      }
+        
+      if(!regexEmail.test(mail.value)){
+        errors += 'INGRESE UN MAIL VALIDO<br>';
+        ok = true
+      }
 
-//form.addEventListener("submit", e=>{
-  //  e.preventDefault()
-    //let advertencia =""
+      if(mensajes.value.length <5){
+        errors += 'MENSAJE MUY CORTO<br>';
+        ok = true
+      } 
+      
+      if(mensajes.value.length >400){
+        errors += 'MENSAJE MUY LARGO<br>';
+        ok = true
+      }
 
+      if(ok){
+        form_error.innerHTML = errors
+        document.getElementById("form_error").style.display ="block";
+        document.getElementById("form_ok").style.display = "none";
+      }
 
-  /*  if(nombre.value.length <5){
-        alert("Debe ingresar nombre completo")} **/
+      if(!ok){
+        document.getElementById("form_error").style.display = "none";
+        document.getElementById("form_ok").style.display= "block";
+        
+      }      
 
-  //  if(regexEmail.test(mail.vulue)){
-    //    alert("El mail ingresado no es valid") }
-    //})
+    
 
+    })
+ 
 
 
